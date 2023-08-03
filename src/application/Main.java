51306598760992e17433974c6c9d1b1fec39072d
WillOli <william.ika@gmail.com>
2023-08-03 20:37:22 -0300

    add class Product

---
+++ src/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc  = new Scanner(System.in);
+
+        Product p = new Product("Tv", 1000.00);
+
+
+
+
+
+
+    }
+}
