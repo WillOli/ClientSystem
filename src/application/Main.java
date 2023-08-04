347fcb54c5fdf2f2b4f6490c0d9ff2025e2c187f
WillOli <william.ika@gmail.com>
2023-08-04 20:28:56 -0300

    end program

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,14 @@
 package application;
 
 import entities.Client;
+import entities.Order;
 import entities.OrderItem;
 import entities.Product;
+import entities.enums.OrderStatus;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -21,10 +24,51 @@ public class Main {
 //        System.out.println(oi1);
 
         /*test 2*/
+//        Client cli = new Client("william", "william@gmail.com", sdf.parse("07/01/1989"));
+//        System.out.println(cli);
 
-        Client cli = new Client("william", "william@gmail.com", sdf.parse("07/01/1989"));
+        /*Final test*/
+        System.out.println("Enter client data");
+        System.out.println("Name: ");
+        String name = sc.nextLine();
+        System.out.println("Email: ");
+        String email = sc.next();
+        System.out.println("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
 
-        System.out.println(cli);
+        Client client = new Client(name, email, birthDate);
+
+
+
+        System.out.println("Enter order data: ");
+        System.out.println("status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.println("How many items to this order? ");
+        int N = sc.nextInt();
+        for (int i=1; i<=N; i++) {
+            System.out.println("Enter #" + (i) + " item data");
+            System.out.println("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.println("Product price: ");
+            double productPrice = sc.nextDouble();
+            System.out.println("Quantity: ");
+            int quantity = sc.nextInt();
+
+            Product product = new Product(productName, productPrice);
+
+            OrderItem it = new OrderItem(quantity, productPrice, product);
+
+            order.addItem(it);
+        }
+
+        System.out.println();
+        System.out.println(order);
+
+
 
 
         sc.close();
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,26 +1,30 @@
 package entities;
 
+import entities.enums.OrderStatus;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Order {
-    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
     private Date moment;
-    private OrderItem status;
+    private OrderStatus status;
+
     private Client client;
+
     private List<OrderItem> items = new ArrayList<OrderItem>();
 
     public Order() {
-
     }
 
-    public Order(Date moment, OrderItem status, Client client) {
+    public Order(Date moment, OrderStatus status, Client client) {
         this.moment = moment;
         this.status = status;
         this.client = client;
-
     }
 
     public Date getMoment() {
@@ -30,15 +34,19 @@ public class Order {
     public void setMoment(Date moment) {
         this.moment = moment;
     }
-    public OrderItem getSatus() {
+
+    public OrderStatus getStatus() {
         return status;
     }
-    public void setSatus(OrderItem status) {
+
+    public void setStatus(OrderStatus status) {
         this.status = status;
     }
+
     public Client getClient() {
         return client;
     }
+
     public void setClient(Client client) {
         this.client = client;
     }
@@ -46,6 +54,7 @@ public class Order {
     public void addItem(OrderItem item) {
         items.add(item);
     }
+
     public void removeItem(OrderItem item) {
         items.remove(item);
     }
@@ -53,7 +62,7 @@ public class Order {
     public double total() {
         double sum = 0.0;
         for (OrderItem it : items) {
-            sum = sum + it.subTotal();
+            sum += it.subTotal();
         }
         return sum;
     }
