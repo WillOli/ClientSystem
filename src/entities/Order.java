b20ad48eff07d6473069482c75505dae7531ec7b
WillOli <william.ika@gmail.com>
2023-08-04 19:09:00 -0300

    implementing the OrderStatus and Order classes

---
+++ src/entities/Order.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private Date moment;
+    private OrderItem satus;
+    private Client client;
+    private List<OrderItem> items = new ArrayList<OrderItem>();
+
+    public Order() {
+
+    }
+
+    public Order(Date moment, OrderItem status, Client client) {
+        this.moment = moment;
+        this.satus = status;
+        this.client = client;
+
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+    public OrderItem getSatus() {
+        return satus;
+    }
+    public void setSatus(OrderItem satus) {
+        this.satus = satus;
+    }
+    public Client getClient() {
+        return client;
+    }
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
